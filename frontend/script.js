5f0344565c0c22e6e6023b61df8772decc479018
silvialupsa <108224053+silvialupsa@users.noreply.github.com>
2023-01-23 14:58:43 +0200

    Details of the selected country

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,30 @@
-//1. List the countries
 const loadEvent = function () {
-    const options = document.querySelector("#all")
-    countries.forEach((country, i) => {
-        options.insertAdjacentHTML("beforeend", `<option id = ${i}> ${country.name.common} </option>`)
-    });
-}
+    //1. List the countries
+    const options = document.querySelector("#all");
 
-window.addEventListener("load", loadEvent)
+    countries.forEach((country, i) => {
+        options.insertAdjacentHTML("beforeend", `<option id = ${i}> ${country.name.common} </option>`);
+    });
+
+    //2. Details of the selected country
+    const main = document.querySelector("#country");
+    options.addEventListener("change", function () {
+        const selectedValue = this.value;
+        const selectedCountry = countries.find(country => country.name.common === selectedValue);
+        if (selectedCountry) {
+            main.innerHTML = `
+                <img src="${selectedCountry.flags.png}">
+                <h1>${selectedCountry.name.common}</h1>
+                <h2>${selectedCountry.region}</h2>
+                <h3>${selectedCountry.subregion}</h3>
+                <h4>${selectedCountry.capital}</h4>
+            `;
+        }
+    });
+
+    //3. Neighbour with the largest population
+    
+
+};
+
+window.addEventListener("load", loadEvent);
